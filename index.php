d9690a77c5a4016146cbe0ed9f3d0d2bbda8f391
danielbacsur <contact@danielbacsur.com>
2022-05-05 10:43:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 <?php
 
 $con = mysqli_connect("localhost","hxdro","12345678","hxdro");
-session_start();
+if ($con -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
 $insert_customer = "insert into accounts (account_name,account_mail,account_pass,account_addr) values ('admin','contact@danielbacsur.com','1234',','prototype')";
-                                                    $run_customer = mysqli_query($con,$insert_customer);
+$run_customer = mysqli_query($con,$insert_customer);
 ?>
 
 
