f632dd8ab8560a381169bf3e60b3e8839d388418
danielbacsur <contact@danielbacsur.com>
2022-05-05 11:09:14 +0200

    db

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost","hxdro","12345678","hxdro");
+if ($con -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
+?>
--- index.php
+++ index.php
@@ -1,44 +1,28 @@
 <!DOCTYPE html>
+<?php include("database.php"); ?>
 <html>
 <head>
 <title>Hxdro</title>
 </head>
 <body>
     
-    <h1>Hxdro - Connect</h1>
-    <p>Kert az otthonodban</p>
-<?php
-
-
-$con = mysqli_connect("localhost","hxdro","12345678","hxdro");
-if ($con -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-  }
-?>
-
-
-<h1>Hxdro - Connect</h1>
-<p>Kert az otthonodban</p>
-
+    <h1>Connect</h1>
 <form action="" method="post">
 NAME: <input type="text" name="name"><br>
 MAIL: <input type="text" name="mail"><br>
 PASS: <input type="text" name="pass"><br>
 ADDR: <input type="text" name="addr"><br>
-<input type="submit" name="register">
+<input type="submit" name="connect">
 
-                                            <?php
-                                            if(isset($_POST['register'])){
-                                                    $name = $_POST['name'];
-                                                    $mail = $_POST['mail'];
-                                                    $pass = $_POST['pass'];
-                                                    $addr = $_POST['addr'];
-                                                    $insert_customer = "insert into accounts (account_name,account_mail,account_pass,account_addr) values ('$name','$mail','$pass','$addr')";
-                                                    $run_customer = mysqli_query($con,$insert_customer);
-                                            }
-                                            ?>
-
+<?php
+if(isset($_POST['connect'])){
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $pass = $_POST['pass'];
+    $addr = $_POST['addr'];
+    $insert_customer = "insert into accounts (account_name,account_mail,account_pass,account_addr) values ('$name','$mail','$pass','$addr')";
+    $run_customer = mysqli_query($con,$insert_customer);
+} ?>
 
 </body>
 </html>
