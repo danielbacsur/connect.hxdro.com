c1d7464c95ee0d02e2f1198b7a8a7611f4defb22
danielbacsur <contact@danielbacsur.com>
2022-05-05 22:46:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(isset($_POST['connect'])){
     $account_name = $_POST['name'];
     $account_email = $_POST['email'];
     $account_pass = $_POST['pass'];
-    $qry_account = "insert into accounts values (UUID(), '$account_name','$account_email','$account_pass')";
+    $qry_account = "insert into accounts values ('$account_id', '$account_name','$account_email','$account_pass')";
     $run_account = mysqli_query($con, $qry_account);
     
     $qry_account = "SELECT * FROM accounts WHERE account_email='$account_email'";
@@ -32,7 +32,7 @@ if(isset($_POST['connect'])){
     $farm_id = guid();
     $farm_apikey = $_GET['farm_apikey'];
     $farm_name = $_POST['farm_name'];
-    $qry_farm = "insert into farms values (UUID(), '$farm_apikey', '$account_id', '$farm_name')";
+    $qry_farm = "insert into farms values ('$farm_id', '$farm_apikey', '$account_id', '$farm_name')";
     $run_farm = mysqli_query($con, $qry_farm);
 
     $sel_farm = "SELECT * FROM farms WHERE farm_apikey='$farm_apikey'";
