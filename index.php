d4d52d3bfdd720a59623ad1bc208bfd4bdafc1f0
danielbacsur <contact@danielbacsur.com>
2022-05-05 21:06:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,25 +26,18 @@ if(isset($_POST['connect'])){
     $run_account = mysqli_query($con, $qry_account);
     $row_account = mysqli_fetch_array($run_account);
     $account_id = $row_account['account_id'];
-    echo $account_id;
     
     $farm_apikey = $_GET['farm_apikey'];
-    echo $farm_apikey;
     $farm_name = $_POST['farm_name'];
     $qry_farm = "insert into farms (farm_apikey, farm_account, farm_name) values ('$farm_apikey', '$account_id', '$farm_name')";
     $run_farm = mysqli_query($con, $qry_farm);
-
 
     $sel_farm = "SELECT * FROM farms WHERE farm_apikey='$farm_apikey'";
     $run_farm = mysqli_query($con, $sel_farm);
     $row_farm = mysqli_fetch_array($run_farm);
     $farm_id = $row_farm['farm_id'];
 
-
-    echo 'farm_id='.$farm_id;
-    echo 'farm_apikey='.$farm_apikey;
-
-    //header( 'Location: http://console.hxdro.com/'.$account_id.'/69420' );
+    header( 'Location: http://console.hxdro.com/'.$account_id.'/'.$farm_id );
 
 } ?>
 
