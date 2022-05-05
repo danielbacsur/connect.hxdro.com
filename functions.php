67bdcccfe779dfc1ac0d39ca58ddd191d865f300
danielbacsur <contact@danielbacsur.com>
2022-05-05 22:12:38 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
-function guid($data)
-{
-    assert(strlen($data) == 16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+function guid() {
+    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(16) : random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // Set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // Set bits 6-7 to 10
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 ?>
