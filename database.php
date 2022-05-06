1ea989f6e38d33c7f54f3d9cb99e1ab5782b4d84
danielbacsur <contact@danielbacsur.com>
2022-05-06 09:10:39 +0200

    asd

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ if ($hxdro -> connect_errno) {
   echo "Failed to connect to MySQL: " . $hxdro -> connect_error;
   exit();
 }
-$cloud = mysqli_connect("localhost","hxdro","12345678","hxdro");
+$cloud = mysqli_connect("localhost","cloud","12345678","cloud");
 if ($cloud -> connect_errno) {
   echo "Failed to connect to MySQL: " . $cloud -> connect_error;
   exit();
